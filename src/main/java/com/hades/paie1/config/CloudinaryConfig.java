a1180ac51ce648df7b753b7c71fa4b248868071d
Ulrichjack <jackeboyeur244@gmail.com>
2025-07-23 15:35:22 +0100

    mise a jour dynamique3

--- src/main/java/com/hades/paie1/config/CloudinaryConfig.java
+++ src/main/java/com/hades/paie1/config/CloudinaryConfig.java
@@ -1,35 +1,31 @@
-package com.hades.paie1.config;
+package com.hades.paie1.config; // Ou le package où se trouve votre configuration Cloudinary
 
 import com.cloudinary.Cloudinary;
-import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
-@Profile("prod")
 public class CloudinaryConfig {
 
-    @Value("${cloudinary.cloud_name}")
+    @Value("${cloudinary.cloud-name}")
     private String cloudName;
 
-    @Value("${cloudinary.api_key}")
+    @Value("${cloudinary.api-key}")
     private String apiKey;
 
-    @Value("${cloudinary.api_secret}")
+    @Value("${cloudinary.api-secret}")
     private String apiSecret;
 
     @Bean
     public Cloudinary cloudinary() {
-        Map<String, String> config = ObjectUtils.asMap(
-                "cloud_name", cloudName,
-                "api_key", apiKey,
-                "api_secret", apiSecret,
-                "secure", true
-        );
+        Map<String, String> config = new HashMap<>();
+        config.put("cloud_name", cloudName);
+        config.put("api_key", apiKey);
+        config.put("api_secret", apiSecret);
         return new Cloudinary(config);
     }
 }
