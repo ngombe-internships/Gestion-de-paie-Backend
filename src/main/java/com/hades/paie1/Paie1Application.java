84b5d0de3e80f44bbc195b1a731a03f47f3bfe99
Ulrichjack <jackeboyeur244@gmail.com>
2025-07-25 16:55:58 +0100

    mise a jour dynamique4

--- src/main/java/com/hades/paie1/Paie1Application.java
+++ src/main/java/com/hades/paie1/Paie1Application.java
@@ -1,11 +1,9 @@
 package com.hades.paie1;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
+;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+
 
 @SpringBootApplication
 public class Paie1Application {
